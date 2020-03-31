643ee2afe9d26b86950b9757eca7103ed6a36fb6
Gabriel Sória <gabrielss.rock@me.com>
2020-03-30 23:34:47 -0300

    Adds function

--- Package.swift
+++ Package.swift
@@ -5,18 +5,16 @@ import PackageDescription
 
 let package = Package(
     name: "CommonMarkExample",
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "CommonMarkExample",
-            dependencies: []),
-        .testTarget(
-            name: "CommonMarkExampleTests",
-            dependencies: ["CommonMarkExample"]),
+            dependencies: ["Ccmark"]),
+            
+        .systemLibrary(
+            name: "Ccmark",
+            pkgConfig: "libcmark",
+            providers: [ .brew(["cmark"]), .apt(["cmark"])
+            ]),
     ]
 )
--- Sources/CommonMarkExample/main.swift
+++ Sources/CommonMarkExample/main.swift
@@ -1,1 +1,13 @@
-print("Hello, world!")
+import Ccmark
+
+let markdown = "*Hello World"
+let cString = cmark_markdown_to_html(markdown, markdown.utf8.count, 0)!
+defer { free(cString) }
+let html = String(cString: cString)
+print(html)
+
+func markdownToHTML(input: String) -> String {
+    let outString = cmark_markdown_to_html(input, input.utf8.count, 0)!
+    defer { free(outString) }
+    return String(cString: outString)
+}
